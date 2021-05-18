17a2047ca2deca0d32f20c3c911c0ede08b8ae56
silviadlc <57746795+silviadlc@users.noreply.github.com>
2021-05-18 18:02:31 +0200

    bug solved

--- public/js/page3java2.js
+++ public/js/page3java2.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
         let solution = datos['solution'];
         solution = solution.toUpperCase();
         if(answer == solution){
-            location.href = "/fourth";
+            location.href = "/search";
         }else{
             alert("RESPUESTA INCORRECTA");
         }
