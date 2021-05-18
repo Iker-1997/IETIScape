9d530afda609d560ef9b959e7416f2207d1aa460
Iker Cayero <iker.1997.19@gmail.com>
2021-05-18 17:06:15 +0200

    (ADD) End functionality and modifies

--- public/js/page5.js
+++ public/js/page5.js
@@ -9,9 +9,15 @@ $(document).ready(function () {
     });
 
     $("#comproveView5").bind("click", function(){
+        let csrf = document
+        .querySelector('meta[name="csrf-token"]')
+        .getAttribute("content");
+
         let answer = $("#inputView5").val();
         answer = answer.toUpperCase();
+    
         let solution = "";
+    
         if (role == "java"){
             solution = datos['solution']['java'];
             solution = solution.toUpperCase();
@@ -19,24 +25,21 @@ $(document).ready(function () {
             solution = datos['solution']['django'];
             solution = solution.toUpperCase();
         }
+    
         if(answer == solution){
-            console.log("ACIERTOOO");
+            fetch("api/endGame/", {
+                method :  "GET",
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Accept": 'application/json',
+                    "X-CSRF-TOKEN": csrf
+                }}).then(function(res) {
+                    location.href = "/end";
+                }).catch(function(err) {
+                    console.error(err)
+                });
         }else{
             alert("RESPUESTA INCORRECTA");
         }
-        /*
-        
-        if (role == "java"){
-            let solution = datos['solution']['java'];
-            solution = solution.toUpperCase();
-        }else{
-            solution = datos['solution']['django'];
-            solution = solution.toUpperCase();
-        }
-        if(answer == solution){
-            console.log("ACIERTOOO");
-        }else{
-            alert("RESPUESTA INCORRECTA");
-        }*/
     });
 });
